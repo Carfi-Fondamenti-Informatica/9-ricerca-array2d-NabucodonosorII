fc4ba33f6a869383a9e3b515c88359732bb76638
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-13 03:01:33 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,8 @@
+#include "lib.h"
+#include <cstring>
 
+bool posizione(char nome[1][20],char elenco[10][20],int &p){
+    for(p=0;p<10;p++){
+        if(strcmp(nome[0],elenco[p])==0)return true;
+    }return false;
+}
