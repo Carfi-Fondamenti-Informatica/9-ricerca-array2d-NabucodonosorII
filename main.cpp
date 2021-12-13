10f6188dbcd5b42378bd0426b00889a2cbcdbe04
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-13 03:01:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char elenco[10][20];char nome[1][20];
+    int n; int p;
+    for (n=0;n<10;n++){
+        cin >> elenco[n];
+    }
+    cin >> nome[0];
+    if(posizione(nome, elenco, p)){
+        cout << p<<endl;
+    }else{
+        cout <<"non presente"<<endl;
+    }
+    return 0;
 }
